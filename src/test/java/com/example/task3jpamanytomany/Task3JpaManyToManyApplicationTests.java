0485a815894fc365b8b2ca287e2870046b2a42fc
Aytac <aramazanli@std.beu.edu.az>
2024-07-10 19:10:08 +0400

    added Task3JpaManyToManyApplicationTests

---
+++ src/test/java/com/example/task3jpamanytomany/Task3JpaManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task3jpamanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task3JpaManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
