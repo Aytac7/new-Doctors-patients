bf472e4613e210919bc9b648d1ab76bec1f2a002
Aytac <aramazanli@std.beu.edu.az>
2024-07-10 19:13:36 +0400

    added MyUserDetailService

---
+++ src/main/java/com/example/task3jpamanytomany/security/MyUserDetailService.java
@@ -0,0 +1,43 @@
+package com.example.task3jpamanytomany.security;
+
+import com.example.task3jpamanytomany.dao.entity.UserEntity;
+import com.example.task3jpamanytomany.dao.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailService implements UserDetailsService {
+
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> user = repository.findByUsername(username);
+        if (user.isPresent()) {
+            var userObj = user.get();
+            return User.builder()
+                    .username(userObj.getUsername())
+                    .password(userObj.getPassword())
+                    .roles(getRoles(userObj))
+                    .build();
+        } else {
+            throw new UsernameNotFoundException(username);
+        }
+    }
+
+    private String[] getRoles(UserEntity user) {
+        if (user.getRole() == null) {
+            return new String[]{"USER"};
+        }
+        return user.getRole().split(",");
+    }
+}
